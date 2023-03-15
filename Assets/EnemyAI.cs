34c512b43aee87f050969507154a6a9b3ea2efe5
dav <73536483+Davizinhn@users.noreply.github.com>
2023-03-14 23:06:46 -0300

    chase

--- Assets/EnemyAI.cs
+++ Assets/EnemyAI.cs
@@ -8,12 +8,45 @@ public class EnemyAI : MonoBehaviour
     public bool canSeguirJogador;
     public NavMeshAgent agent;
     public GameObject Player;
+    public AudioSource aud;
+    public AudioClip[] chases;
+    bool toqueiChase;
 
     void Update()
     {
         if(canSeguirJogador)
         {
             agent.SetDestination(Player.transform.position);
+        }
+        ChaseAudioControl();
+    }
+
+    void ChaseAudioControl()
+    {
+        if(canSeguirJogador)
+        {
+            if(!aud.isPlaying && !toqueiChase)
+            {
+                aud.clip=chases[0];
+                aud.Play();
+                aud.loop=false;
+                toqueiChase=true;
+            }else if(!aud.isPlaying && toqueiChase)
+            {
+                if(agent.remainingDistance > 15)
+                {
+                    aud.clip=chases[1];
+                }
+                else if(agent.remainingDistance < 15 && agent.remainingDistance > 10)
+                {
+                    aud.clip=chases[2];
+                }
+                else if(agent.remainingDistance < 10)
+                {
+                    aud.clip=chases[3];
+                }
+                aud.Play();
+            }
         }
     }
 }
