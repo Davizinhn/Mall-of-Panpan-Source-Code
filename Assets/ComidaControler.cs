c209fc7ac6d50e63c05bd7190755db0effa2efa2
dav <73536483+Davizinhn@users.noreply.github.com>
2023-03-17 18:03:08 -0300

    a

--- Assets/ComidaControler.cs
+++ Assets/ComidaControler.cs
@@ -10,7 +10,9 @@ public class ComidaControler : MonoBehaviour
     public bool canAdd = true;
     public AudioSource tummmm;
     public GameObject portaparapoder;
+    public Animator anim;
     public bool jato;
+    public GameObject brownCard;
 
     public void Update()
     {   
@@ -24,13 +26,17 @@ public class ComidaControler : MonoBehaviour
 
     IEnumerator abrirportinha()
     {
+                yield return new WaitForSeconds(2.75f);
+                anim.SetTrigger("Cuspir");
         yield return new WaitForSeconds(3f);
         // coisas
         portaparapoder.tag="Porta";
-        this.gameObject.GetComponent<EnemyAI>().canSeguirJogador=true;
+        //this.gameObject.GetComponent<EnemyAI>().canSeguirJogador=true;
+        GameObject.Find("TipManager").GetComponent<TipManager>().ShowItem("Brown Card x1");
         this.gameObject.tag="Untagged";
-        this.gameObject.active=false;
-                this.gameObject.active=true;
+                new WaitForSeconds(0.75f);
+                brownCard.active=false;
+                
         StopCoroutine(abrirportinha());
     }
 
@@ -38,6 +44,7 @@ public class ComidaControler : MonoBehaviour
     {
         canAdd=false;
         tummmm.Play();
+        anim.SetTrigger("Comer");
         tenhoesses = tenhoesses+1;
         StartCoroutine(podeVoltar());
     }
