16ba31bff4463090ee9eafabf3cd6dbeed80c1dc
dav <73536483+Davizinhn@users.noreply.github.com>
2023-03-16 23:58:36 -0300

    menu

---
+++ Assets/MenuManager.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using TMPro;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject Loader;
+    public Button[] buttons;
+    public AudioSource[] aud;
+    bool ha = false;
+
+    public void Jogar()
+    {
+        StartCoroutine(fazerLoad("Jogar"));
+    }
+
+    public void Quit()
+    {
+        StartCoroutine(fazerLoad("Quit"));
+    }
+
+    public void Update()
+    {
+        if(ha)
+        {
+                foreach(AudioSource i in aud)
+        {
+            i.volume = i.volume-0.005f;
+        }
+        }
+    }
+
+    public IEnumerator fazerLoad(string acao)
+    {
+        ha=true;
+                foreach(Button b in buttons)
+        {
+            b.interactable = false;
+        }
+        Loader.SetActive(true);
+        yield return new WaitForSeconds(2f);
+        if(acao == "Jogar")
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+        else if(acao=="Quit")
+        {
+            Application.Quit();
+        }
+    }
+}
